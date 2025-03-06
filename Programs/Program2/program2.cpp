2badcc1da4918b7aba39a1b239eb9907d5c4b1c1
MalachiDorman <malachi.dorman@gmail.com>
2025-03-06 14:17:06 -0600

    update

--- Programs/Program2/program2.cpp
+++ Programs/Program2/program2.cpp
@@ -1,3 +1,13 @@
+/**************************************************
+* Name: Malachi Dorman                            *
+* Class: CSC 1300-002                             *
+* Assignment: Program 2                           *
+* Purpose: A Jedi Database that can add, remove   *
+* or view jedis.                                  *
+* Date: 2/26/2025                                 *                                  
+* Modified: 3/5/2025                              *
+***************************************************/
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -6,6 +16,7 @@ using namespace std;
 int main() {
     int selection;
     string fileName = "jedi.txt";
+    string tempFileName = "temp.txt";
     string jediName;
     string planet;
     string saber;
@@ -16,14 +27,15 @@ int main() {
         cout << "What would you like to do today?\n";
         cout << "1. Add a Jedi\n";
         cout << "2. Display all Jedi\n";
-        cout << "3. Remove all Jedi (clear file)\n";
+        cout << "3. Remove a Jedi\n";
         cout << "4. Exit\n";
         cout << "Please choose an option: ";
 
         cin >> selection;
         cin.ignore(); 
 
-        if (selection == 1) { // Add Jedi
+        // Add Jedi
+        if (selection == 1) { 
             ofstream outFile(fileName, ios::app); 
             if (outFile.is_open()) {
                 cout << "Enter the Jedi's name: ";
@@ -42,12 +54,13 @@ int main() {
             }
         }
 
-        else if (selection == 2) { // Display all Jedi
+        // Display all Jedi
+        else if (selection == 2) { 
             ifstream inFile(fileName);
             if (inFile.is_open()) {
                 cout << "\nJedi currently in the Jedi Archive:\n";
                 int count = 1;
-                while (inFile >> jediName >> planet >> saber) {
+                while (getline(inFile, jediName, ':') && getline(inFile, planet, '|') && getline(inFile, saber)) {
                     cout << count << ". " << jediName << " from " << planet
                          << " that has a " << saber << " lightsaber.\n";
                     count++;
@@ -58,15 +71,65 @@ int main() {
             }
         }
 
-        else if (selection == 3) { // Remove a Jedi
-            
+        // Remove a Jedi
+        else if (selection == 3) { 
+            ifstream inFile(fileName);
+
+            cout << "\nJedi currently in the Jedi Archive:\n";
+            int count = 1, totalJedi = 0;
+            string line;
+            while (getline(inFile, line)) {
+                cout << count << ". " << line << endl;
+                count++;
+                totalJedi++;
+            }
+            inFile.close();
+        
+        // If there are no jedi in database to remove
+        if (totalJedi == 0) {
+            cout << "No Jedi to remove.\n";
+            continue;
+        }
+    
+
+    // Var for removing Jedi
+    int removeJedi;
+    cout << "Enter the number of the Jedi to remove: ";
+    cin >> removeJedi;
+
+    // While for invalid choice
+    while (cin.fail() || removeJedi < 1 || removeJedi > totalJedi) {
+        cin.clear();
+        cin.ignore();
+        cout << "Invalid choice. Enter a valid number: ";
+        cin >> removeJedi;
+    }
+
+    inFile.open(fileName);
+    ofstream tempFile(tempFileName);
+    count = 1;
+    while (getline(inFile, line)) {
+        if (count != removeJedi) {
+            tempFile << line << endl;
+        }
+        count++;
+    }
+    inFile.close();
+    tempFile.close();
+
+    // Replaces the temp.txt with updated jedi.txt
+    remove("jedi.txt");
+    rename("temp.txt", "jedi.txt");
+
         }
 
-        else if (selection == 4) { // Exit
+        // Exit
+        else if (selection == 4) { 
             cout << "Thank you for helping the cause! Have a great day! May the Force be with you!!\n";
         }
 
-        else { // Invalid option
+        // Invalid option
+        else { 
             cout << "Invalid option.\n";
         }
     } while (selection != 4);
